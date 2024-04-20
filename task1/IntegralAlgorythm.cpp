e08c89a45e68ba175c2d32402719a875d0d660c6
eugeniisenyush <senusheugen@gmail.com>
2024-04-20 23:29:42 +0200

    [task1] Base class for families of integration algorithms

---
+++ task1/IntegralAlgorythm.cpp
@@ -0,0 +1,7 @@
+#include "Polynomial.cpp"
+
+class IntegralAlgorythm {
+    public: 
+        virtual ~IntegralAlgorythm() = default;
+        virtual double integration(Polynomial f, int a, int b);
+};
