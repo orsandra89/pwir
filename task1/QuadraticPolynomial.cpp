c6307d3028b83f4ef29b2b84b561634f91192cff
eugeniisenyush <senusheugen@gmail.com>
2024-04-20 22:54:25 +0200

    [task1] create model to represent quadratic polinomial

---
+++ task1/QuadraticPolynomial.cpp
@@ -0,0 +1,25 @@
+class QuadraticPolynomial{
+    private:
+        int quadraticCoefficient;
+        int linearCoefficient;
+        int constant;
+
+    public:
+        QuadraticPolynomial(int a, int b, int c) {
+            quadraticCoefficient = a;
+            linearCoefficient = b;
+            constant = c;
+        }
+
+        int getQuadraticCoefficient() {
+            return quadraticCoefficient;
+        }
+
+        int getLinearCoefficient() {
+            return linearCoefficient;
+        }
+
+        int getConstant() {
+            return constant;
+        }
+};
