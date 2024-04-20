95853bd7eefa7f43ce0f965b21e0636a5271f3a8
eugeniisenyush <senusheugen@gmail.com>
2024-04-20 23:23:38 +0200

    [task1] refactor polynomial model

--- task1/Polynomial.cpp
+++ task1/Polynomial.cpp
@@ -1,4 +1,13 @@
+#include <vector>
+
 class Polynomial {
+    private: 
+        std::vector<int> coefficients;
+
     public:
+        Polynomial(std::vector<int> coeffs) : coefficients(coeffs) {}
         virtual ~Polynomial() = default;
+        std::vector<int> getCoefficients() {
+            return coefficients;
+        }
 };
--- task1/QuadraticPolynomial.cpp
+++ task1/QuadraticPolynomial.cpp
@@ -1,27 +1,6 @@
 #include "Polynomial.cpp"
 
 class QuadraticPolynomial : public Polynomial {
-    private:
-        int quadraticCoefficient;
-        int linearCoefficient;
-        int constant;
-
     public:
-        QuadraticPolynomial(int a, int b, int c) {
-            quadraticCoefficient = a;
-            linearCoefficient = b;
-            constant = c;
-        }
-
-        int getQuadraticCoefficient() {
-            return quadraticCoefficient;
-        }
-
-        int getLinearCoefficient() {
-            return linearCoefficient;
-        }
-
-        int getConstant() {
-            return constant;
-        }
+        QuadraticPolynomial(int a, int b, int c) : Polynomial({c, b, a}) {}
 };
