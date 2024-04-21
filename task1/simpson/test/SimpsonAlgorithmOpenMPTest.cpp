8ef580774263b987025cee0608b2edaadbed7155
eugeniisenyush <senusheugen@gmail.com>
2024-04-21 19:56:17 +0200

    [task1] test suppert for simpson algorithm with open mp support

---
+++ task1/simpson/test/SimpsonAlgorithmOpenMPTest.cpp
@@ -0,0 +1,19 @@
+#include "SimpsonAlgorithmOpenMPTest.h"
+
+SimpsonAlgorithmOpenMPTest::SimpsonAlgorithmOpenMPTest(Polynomial* poly, double a, double b, std::string filename) : IntegralTest(poly, a, b, filename) {
+    this->poly = poly;
+    this->a = a;
+    this->b = b;
+    this->filename = filename;
+    this->algorithm = SimpsonAlgorithmOpenMP();
+}
+
+CsvRecord SimpsonAlgorithmOpenMPTest::test(int num_intervals) {
+    auto start = std::chrono::steady_clock::now();
+    
+    algorithm.integration(*poly, a, b, num_intervals);
+
+    auto end = std::chrono::steady_clock::now();
+    double elapsed_time = std::chrono::duration<double>(end - start).count();
+    return { "SimpsonAlgorithmOpenMP", elapsed_time, num_intervals };
+}
