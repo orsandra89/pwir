3be7aaa52caf6e8f99447c0f87f691b6b2c4088a
eugeniisenyush <senusheugen@gmail.com>
2024-05-04 22:56:35 +0200

    [task2] test execution file which takes parameters from command line and execute matrix multiplication. also print calculation time.

--- task2/test.cpp
+++ task2/test.cpp
@@ -1,27 +1,93 @@
 #include "Matrix.h"
+#include <map>
+#include <cmath>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <matrix1_filename> <matrix2_filename>" << std::endl;
+    std::map<int, std::string> algorithmMap = {
+        {0, "Sequential"},
+        {1, "OpenMP"}
+    };
+    if (argc < 4) {
+        std::cout << "Available integration algorithms:" << std::endl;
+        for (const auto& pair : algorithmMap) {
+            std::cout << pair.first << ". " << pair.second << std::endl;
+        }
+        std::cerr << "Usage: " << argv[0] << " <matrix1_filename> <matrix2_filename> <algorithm> <result_filename> <thread_num>" << std::endl;
         return 1;
     }
 
     std::string fileName1 = argv[1];
     std::string fileName2 = argv[2];
+    int algorithm = std::stoi(argv[3]);
 
-    Matrix mat1(fileName1);
-    Matrix mat2(fileName2);
+    std::string resultFileName2 = argv[4];
 
-    std::cout << "Matrix loaded from " << fileName1 << ":" << std::endl;
-    mat1.print();
+    if (algorithm == 0){
 
-    std::cout << "Matrix loaded from " << fileName2 << ":" << std::endl;
-    mat2.print();
+        Matrix mat1(fileName1);
+        Matrix mat2(fileName2);
 
-    Matrix r = mat1 * mat2;
+        std::cout << "Matrix loaded from " << fileName1 << ":" << std::endl;
+        mat1.print();
 
-    std::cout << "Result matrix:" << std::endl;
-    r.print();
+        std::cout << "Matrix loaded from " << fileName2 << ":" << std::endl;
+        mat2.print();
+
+        auto start = std::chrono::steady_clock::now();
+        
+        Matrix result = mat1 * mat2;
+
+        auto end = std::chrono::steady_clock::now();
+        double elapsed_time = std::chrono::duration<double>(end - start).count();
+
+        std::cout << "Result matrix:" << std::endl;
+        result.print();
+
+        std::cout << "Calculation time: " << elapsed_time << std::endl;
+
+        result.writeToFile(resultFileName2);
+    } else if (algorithm == 1) {
+        if (argc != 6) {
+            std::cout << "Available integration algorithms:" << std::endl;
+            for (const auto& pair : algorithmMap) {
+                std::cout << pair.first << ". " << pair.second << std::endl;
+            }
+            std::cerr << "Usage: " << argv[0] << " <matrix1_filename> <matrix2_filename> <algorithm> <result_filename> <thread_num>" << std::endl;
+            return 1;
+        }
+
+        int numThread = std::stoi(argv[5]);
+
+        Matrix mat1(fileName1);
+        Matrix mat2(fileName2);
+
+        std::cout << "Matrix loaded from " << fileName1 << ":" << std::endl;
+        mat1.print();
+
+        std::cout << "Matrix loaded from " << fileName2 << ":" << std::endl;
+        mat2.print();
+
+        auto start = std::chrono::steady_clock::now();
+        
+        Matrix result = mat1.matrixMultiply(mat2, numThread);
+
+        auto end = std::chrono::steady_clock::now();
+        double elapsed_time = std::chrono::duration<double>(end - start).count();
+
+        std::cout << "Result matrix:" << std::endl;
+        result.print();
+
+        std::cout << "Calculation time: " << elapsed_time << std::endl;
+
+        result.writeToFile(resultFileName2);
+    } else {
+        std::cout << "Available integration algorithms:" << std::endl;
+            for (const auto& pair : algorithmMap) {
+                std::cout << pair.first << ". " << pair.second << std::endl;
+            }
+            std::cerr << "Usage: " << argv[0] << " <matrix1_filename> <matrix2_filename> <algorithm> <result_filename> <thread_num>" << std::endl;
+            return 1;
+    }
 
     return 0;
 }
